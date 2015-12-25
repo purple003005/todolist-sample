f0a0569f7dc1590ecc209a80539b27e90886c3f8
Kay <purple003005@gmail.com>
2015-12-26 01:01:15 +0800

    improve react todolist

--- react_todolist/app.js
+++ react_todolist/app.js
@@ -1,80 +1,103 @@
-var Add = React.createClass({
-  getInitialState:function(){
-    return (
-        {todoList : []}
-    );
-  },
-  addTodoState:function(list){
-    var addList = this.state.todoList;
-    addList.push(list);
-    this.setState({'todoList': addList});
-  },
-  render: function() {
-    return (
-        <li>
-          <InputEvent addTodos={this.addTodoState}/>
-            <RenderList todolistState={this.state.todoList}/>
-        </li>
+var App = React.createClass({
+    getInitialState:  function(){
+        return {todoList : [{val:'123',onEdit:false}]}
 
-    );
-  }
+    },
+    keyEnter:function(e){
+        if(e.keyCode==13){
+            this.add();
+        }
+    },
+    add:function(e){
+        var temp = this.state.todoList;
+        var textNode = ReactDOM.findDOMNode(this.refs.add).value;
+        if (textNode===''){
+            this.setState({emptyInput:true});
+        }else{
+            var obj={val:textNode,onEdit:false}
+            temp.push(obj);
+            this.setState({'todoList':temp})
+            ReactDOM.findDOMNode(this.refs.add).value='';
+            this.setState({emptyInput:false});
+        }
+    },
+
+    edit: function(index) {
+        var temp = this.state.todoList;
+        temp[index].onEdit = true;
+        this.setState({'todoList':temp});
+    },
+    del: function(index) {
+        var temp = this.state.todoList;
+        temp.splice(index,1);
+        this.setState({'todoList':temp});
+    },
+    ok: function(index){
+        //var changeTemp = this.state.changeVal;
+
+        var change = this.state.changeState;
+        var temp = this.state.todoList;
+        if(change){
+            var newVal = this.state.newValue;
+            console.log(newVal);
+            var obj={val:newVal,onEdit:false}
+            temp[index] = obj;
+            this.setState({changeState:false})
+        }else{
+            temp[index].onEdit=false;
+        }
+        this.setState({'todoList':temp});
+
+    },
+    cancel: function(index){
+        var temp = this.state.todoList;
+        temp[index].onEdit = false;
+        this.setState({'todoList':temp});
+    },
+    onChange: function(e)
+    {
+        var newVal = e.target.value;
+        this.setState({ newValue:newVal,changeState:true})
+    },
+    componentWillMount: function() {
+        console.log('componnentWillMount()');
+
+    },
+    componentDidMount: function() {
+        console.log('componentDidMound');
+    },
+    render: function() {
+        var _this = this;
+        var empty = this.state.emptyInput;
+        var inputCss='spanteam';
+        if(empty) inputCss = 'empty'
+        var list = this.state.todoList.map(function(val, idx){
+            var classes = 'liteam';
+            if (val.onEdit) classes += ' edit';
+            else if (!val.onEdit) classes += ' add';
+            return (
+                    <li className={classes} ref='item' key={idx}><span className='spanteam'>{val.val}</span>
+                    <input className='inputteam' ref='input' type="text" defaultValue={val.val} onChange={this.onChange}/>
+                    <button className='editbtn' onClick={this.edit.bind(this,idx)}>edit</button>
+                    <button className='removebtn' onClick={this.del.bind(this,idx)}>remove</button>
+                    <button className='okbtn' onClick={this.ok.bind(this,idx)}>ok</button>
+                    <button className='cancelbtn' onClick={this.cancel.bind(this,idx)}>cancel</button></li>
+
+            );
+        }, this);
+        //console.log(list)
+        return (
+            <div>
+                <input className={inputCss} ref="add" type="text" onKeyDown={this.keyEnter}/><button className='addbtn' onClick={this.add} >add</button>
+                <ul>
+
+                    {list}
+                </ul>
+            </div>
+
+        );
+
+    }
 });
 
-var RenderList = React.createClass({
-    getInitialState:function() {
-        return (
-            {'todoList': this.props.todolistState}
-        );
-    },
-    componentWillReceiveProps:function(newProps){
-        this.setState({'todoList':newProps.todolistState});
-        //debugger;
-    },
-    /*
-    handleEdit:function(e){
-      console.log('handleEdit');
-    },
-    */
-    render:function(){
-        var tmplist = this.state.todoList.map(function(list,index){
-            return(
-                <li key={index}>
-                    {list}
-                    <button >
-                        Edit
-                    </button>
-                    <button>
-                        Remove
-                    </button>
-                </li>
-            )
-        })
-        return(
-            <ul>
-                {tmplist}
-            </ul>
-        )
-    }
-
-})
-/*
-var Editlist = React.createClass({
-
-})
-*/
-var InputEvent = React.createClass({
-  KeyEnter:function(e){
-    if(e.keyCode==13){
-      console.log(ReactDOM.findDOMNode(this).value);
-      this.props.addTodos(ReactDOM.findDOMNode(this).value);
-        ReactDOM.findDOMNode(this).value = '';
-    }
-  },
-  render:function(){
-    return(
-        <input placeholder='Type Something...' type="text" onKeyDown={this.KeyEnter}/>
-    )
-  }
-})
-
-ReactDOM.render(<Add />,document.getElementById('addTxt'));
+ReactDOM.render(<App />,document.getElementById('addTxt'));
---
+++ react_todolist/app_v2.js
@@ -0,0 +1,71 @@
+/**
+ * Created by kay on 15/12/21.
+ */
+var Todo = React.createClass({
+    displayName:'TODO',
+    getInitialState:function(){
+        return(
+        {
+            todos:[]
+        }
+        );
+    },
+    addTodoHandler:function(todo){
+        var tmpTodos = this.state.todos
+        tmpTodos.push(todo);
+        this.setState({'todos':tmpTodos});
+    },
+    render:function(){
+        return(
+            <div>
+                <h1>Todo</h1>
+                <TodoInputField addTodoHandler={this.addTodoHandler}/>
+                <TodoContent todos={this.state.todos} />
+            </div>
+        );
+    }
+});
+var TodoContent = React.createClass({
+    displayName:'TODO_CONTENT',
+    getInitialState:function(){
+        return(
+        {
+            'todos':this.props.todos
+        }
+        );
+    },
+    componentWillReceiveProps:function(nextProps){
+        this.setState({'todos':nextProps.todos});
+    },
+    render:function(){
+        var todos = _.map(this.state.todos,function(todo,idx){
+            return(
+                <li key = {idx}>{todo}</li>
+            );
+        });
+        return(
+            <ul>
+                {todos}
+            </ul>
+        );
+    }
+});
+var TodoInputField = React.createClass({
+    displayName:'TODO_INPUT_FILED',
+    addTodo:function(e){
+        if(e.keyCode == 13){
+            var inputValue = ReactDOM.findDOMNode(this).value;
+            var timestamp = ' @ ' + (new Date()).toLocaleString()
+            this.props.addTodoHandler(inputValue + timestamp);
+            ReactDOM.findDOMNode(this).value = '';
+        }
+    },
+    render:function(){
+        return(
+            <input placeholder='Type Something' type="text" onKeyDown={this.addTodo}/>
+        );
+
+    }
+});
+
+ReactDOM.render(<Todo />,document.querySelector('#app'));
