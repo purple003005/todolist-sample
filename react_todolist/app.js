d13b6e63cc5bfa301890d1cd9003e46519c47108
Kay <purple003005@gmail.com>
2015-12-30 14:05:36 +0800

    revise input value add localStorage and onSubmit

--- react_todolist/app.js
+++ react_todolist/app.js
@@ -1,15 +1,10 @@
 var App = React.createClass({
     getInitialState:  function(){
         return {todoList : [{val:'123',onEdit:false}]}
-
-    },
-    keyEnter:function(e){
-        if(e.keyCode==13){
-            this.add();
-        }
     },
     add:function(e){
-        var temp = this.state.todoList;
+        e.preventDefault();
+        var temp =JSON.parse(localStorage.getItem("todos"));
         var textNode = ReactDOM.findDOMNode(this.refs.add).value;
         if (textNode===''){
             this.setState({emptyInput:true});
@@ -19,45 +14,46 @@ var App = React.createClass({
             this.setState({'todoList':temp})
             ReactDOM.findDOMNode(this.refs.add).value='';
             this.setState({emptyInput:false});
+            localStorage.setItem("todos",JSON.stringify(temp));
         }
     },
 
     edit: function(index) {
-        var temp = this.state.todoList;
+        var temp =JSON.parse(localStorage.getItem("todos"));
+        var oldVal = temp[index].val;
         temp[index].onEdit = true;
-        this.setState({'todoList':temp});
+        this.setState({'todoList':temp,'oldVal':oldVal});
+        localStorage.setItem("todos",JSON.stringify(temp));
     },
     del: function(index) {
-        var temp = this.state.todoList;
+        var temp =JSON.parse(localStorage.getItem("todos"));
         temp.splice(index,1);
         this.setState({'todoList':temp});
+        localStorage.setItem("todos",JSON.stringify(temp));
     },
     ok: function(index){
-        //var changeTemp = this.state.changeVal;
-
-        var change = this.state.changeState;
-        var temp = this.state.todoList;
-        if(change){
-            var newVal = this.state.newValue;
-            console.log(newVal);
-            var obj={val:newVal,onEdit:false}
-            temp[index] = obj;
-            this.setState({changeState:false})
-        }else{
-            temp[index].onEdit=false;
-        }
+        var temp =JSON.parse(localStorage.getItem("todos"));
+        temp[index].onEdit=false;
         this.setState({'todoList':temp});
+        localStorage.setItem("todos",JSON.stringify(temp));
 
     },
     cancel: function(index){
-        var temp = this.state.todoList;
+        var temp =JSON.parse(localStorage.getItem("todos"));
+        var old = this.state.oldVal;
         temp[index].onEdit = false;
+        temp[index].val = old;
         this.setState({'todoList':temp});
+        localStorage.setItem("todos",JSON.stringify(temp));
     },
-    onChange: function(e)
+    onChange: function(index,e)
     {
+        var temp =JSON.parse(localStorage.getItem("todos"));
         var newVal = e.target.value;
-        this.setState({ newValue:newVal,changeState:true})
+        temp[index].val= newVal;
+        this.setState({ 'todoLsit' : temp})
+        localStorage.setItem("todos",JSON.stringify(temp));
+
     },
     componentWillMount: function() {
         console.log('componnentWillMount()');
@@ -67,28 +63,31 @@ var App = React.createClass({
         console.log('componentDidMound');
     },
     render: function() {
+        console.log('render');
         var _this = this;
         var empty = this.state.emptyInput;
         var inputCss='spanteam';
         if(empty) inputCss = 'empty'
-        var list = this.state.todoList.map(function(val, idx){
+        var tmp =JSON.parse(localStorage.getItem("todos"));
+        var list = tmp.map(function(val, idx){
             var classes = 'liteam';
             if (val.onEdit) classes += ' edit';
             else if (!val.onEdit) classes += ' add';
             return (
                     <li className={classes} ref='item' key={idx}><span className='spanteam'>{val.val}</span>
-                    <input className='inputteam' ref='input' type="text" defaultValue={val.val} onChange={this.onChange}/>
+                    <input className='inputteam' ref='input' type="text" value={val.val} onChange={this.onChange.bind(this,idx)}/>
                     <button className='editbtn' onClick={this.edit.bind(this,idx)}>edit</button>
                     <button className='removebtn' onClick={this.del.bind(this,idx)}>remove</button>
                     <button className='okbtn' onClick={this.ok.bind(this,idx)}>ok</button>
                     <button className='cancelbtn' onClick={this.cancel.bind(this,idx)}>cancel</button></li>
-
             );
         }, this);
         //console.log(list)
         return (
             <div>
-                <input className={inputCss} ref="add" type="text" onKeyDown={this.keyEnter}/><button className='addbtn' onClick={this.add} >add</button>
+                <form onSumit={this.add}>
+                    <input className={inputCss} ref="add" type="text" onKeyDown={this.keyEnter}/><button className='addbtn' onClick={this.add} >add</button>
+                </form>
                 <ul>
 
                     {list}
